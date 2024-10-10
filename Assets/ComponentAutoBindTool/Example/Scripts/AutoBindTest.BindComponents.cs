f498f66bff0379ee362119a6ee6f05cc4a62db82
Shuai <45944672+q353922918@users.noreply.github.com>
2024-10-11 00:27:13 +0800

    补充提交

--- Assets/ComponentAutoBindTool/Example/Scripts/AutoBindTest.BindComponents.cs
+++ Assets/ComponentAutoBindTool/Example/Scripts/AutoBindTest.BindComponents.cs
@@ -3,18 +3,18 @@ using AutoBindTool.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
 
-//自动生成于：2024/10/10 23:51:49
+//自动生成于：2024/10/11 0:02:39
 	public partial class AutoBindTest
 	{
 
 		[Serializable]
 		public class UIView
 		{
-			public Image imgTest1;
 			public Button btnTest2;
-			public Text txtTest3;
 			public Dropdown dropTest4;
+			public Image imgTest1;
 			public Image imgTest4;
+			public Text txtTest3;
 		}
 		/// <summary>
 		/// ========== UI组件 ==========
@@ -27,11 +27,11 @@ using UnityEngine.UI;
 
 			view = new UIView
 			{
-				imgTest1 = autoBindTool.GetBindComponent<Image>(0),
-				btnTest2 = autoBindTool.GetBindComponent<Button>(1),
-				txtTest3 = autoBindTool.GetBindComponent<Text>(2),
-				dropTest4 = autoBindTool.GetBindComponent<Dropdown>(3),
-				imgTest4 = autoBindTool.GetBindComponent<Image>(4),
+				btnTest2 = autoBindTool.GetBindComponent<Button>(0),
+				dropTest4 = autoBindTool.GetBindComponent<Dropdown>(1),
+				imgTest1 = autoBindTool.GetBindComponent<Image>(2),
+				imgTest4 = autoBindTool.GetBindComponent<Image>(3),
+				txtTest3 = autoBindTool.GetBindComponent<Text>(4),
 			};
 		}
 	}
